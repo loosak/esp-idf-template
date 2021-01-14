11f3b766f5f331f297cd64591d668d19844c8ab1
kcoufal <kcoufal@gmail.com>
2021-01-14 21:14:00 +0100

    added nvi logging

--- main/main.c
+++ main/main.c
@@ -1,14 +1,27 @@
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "nvs_flash.h"
+#include "esp_log.h"
+
+static const char *TAG = "app_main";
 
 void app_main(void)
 {
-    int i = 0;
-    while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    esp_log_level_set(TAG, ESP_LOG_VERBOSE);
+    ESP_LOGI(TAG, "Running at %s:%d", __FILE__, __LINE__);
+    
+    uint16_t initial_tasks = uxTaskGetNumberOfTasks();
+    ESP_LOGI(TAG, "Number of running tasks: %d", initial_tasks);
+
+    //Initialize NVS --- 
+    esp_err_t ret = nvs_flash_init();
+    if(ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
     }
+    ESP_ERROR_CHECK(ret);
+
+
 }
